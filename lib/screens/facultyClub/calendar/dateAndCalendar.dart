6219d061d9b3ac3fbebb0364146236eca02b99e9
eleminatorcode <115079598+eleminatorcode@users.noreply.github.com>
2024-04-24 22:47:34 +0530

    FacultyClubCalendar Pushed

---
+++ lib/screens/facultyClub/calendar/dateAndCalendar.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+class DateAndCalendar extends StatefulWidget {
+  const DateAndCalendar({super.key});
+
+  @override
+  State<DateAndCalendar> createState() => _DateAndCalendarState();
+}
+
+class _DateAndCalendarState extends State<DateAndCalendar> {
+  DateTime selectedDate = DateTime.now();
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    String months = new DateFormat('MMM').format(selectedDate);
+    String years = new DateFormat('y').format(selectedDate);
+    String days = new DateFormat('d').format(selectedDate);
+    String weeks = new DateFormat('EEEE').format(selectedDate);
+    return Column(
+      children: [
+        Container(
+          height: size.height * 0.066,
+          width: size.width * 0.30,
+          decoration: BoxDecoration(
+              color: Colors.red,
+              borderRadius:
+              BorderRadius.vertical(top: Radius.circular(10))),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Text(
+                months,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 15,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                years,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 15,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+        ),
+        ElevatedButton(
+          onPressed: () async {
+            final DateTime? pickedDate = await showDatePicker(
+                context: context,
+                initialDate: selectedDate,
+                firstDate: DateTime(2000),
+                lastDate: DateTime(2025));
+            if (pickedDate != null) {
+              setState(() {
+                selectedDate = pickedDate;
+              });
+            };
+          },
+          
+
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Text(
+                "${days} ",
+                style: TextStyle(fontSize:21,color: Colors.white),
+              ),
+              Text(
+                "${weeks} ",
+                style:
+                    TextStyle(fontWeight: FontWeight.bold, fontSize:12,color: Colors.white),
+              ),
+            ],
+          ),
+          style: ElevatedButton.styleFrom(
+            fixedSize: Size(size.width*0.30, size.height*0.13),
+            padding: EdgeInsets.all(5),
+            backgroundColor: Colors.grey.shade900,
+            shape: RoundedRectangleBorder(
+              borderRadius:
+                  BorderRadius.vertical(bottom: Radius.circular(8)),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
---
+++ lib/screens/facultyClub/facultyClubCalendar.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:studentapp/screens/facultyClub/calendar/dateAndCalendar.dart';
+import 'package:studentapp/screens/facultyClub/faculty/faculty.dart';
+import 'package:studentapp/screens/facultyClub/socities/societies.dart';
+
+class FacultyClubCalendar extends StatefulWidget {
+  const FacultyClubCalendar({super.key});
+
+  @override
+  State<FacultyClubCalendar> createState() => _FacultyClubCalendarState();
+}
+
+class _FacultyClubCalendarState extends State<FacultyClubCalendar> {
+  bool toggle_switch = true;
+  bool is_SocietyColor = true;
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.fromLTRB(15, 0, 0, 0),
+          child: Row(
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    is_SocietyColor = !is_SocietyColor;
+                    toggle_switch = true;
+                  });
+                },
+                child: Text("SOCIETY",
+                    style: TextStyle(
+                        color: is_SocietyColor ? Colors.white : Colors.black,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 13)),
+                style: ElevatedButton.styleFrom(
+                    padding: EdgeInsets.all(0),
+                    fixedSize: Size(size.width * 0.25, 5),
+                    backgroundColor:
+                        is_SocietyColor ? Colors.grey[800] : Colors.grey[300]),
+              ),
+               SizedBox(
+                 width: 10,
+               ),
+              ElevatedButton(
+                onPressed: () {
+                  setState(() {
+                    is_SocietyColor = !is_SocietyColor;
+                    toggle_switch = false;
+                  });
+                },
+                child: Text(
+                  "FACULTY",
+                  style: TextStyle(
+                      color: is_SocietyColor ? Colors.black : Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 13),
+                ),
+                style: ElevatedButton.styleFrom(
+                    padding: EdgeInsets.only(left: 0),
+                    fixedSize: Size(size.width * 0.25, 5),
+                    backgroundColor:
+                        is_SocietyColor ? Colors.grey[300] : Colors.grey[800]),
+              ),
+            ],
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              is_SocietyColor ? Socities() : Faculty(),
+              DateAndCalendar(),
+            ],
+          ),
+        )
+      ],
+    );
+  }
+}
