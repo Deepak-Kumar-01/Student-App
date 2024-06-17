fd9e4bd8f43f28797e2aaba538cc24db88d84922
eleminatorcode <115079598+eleminatorcode@users.noreply.github.com>
2024-06-17 21:05:50 +0530

    Assignment list make difference through color of subjets

--- lib/screens/facultyClub/Assignment/assignmentlist.dart
+++ lib/screens/facultyClub/Assignment/assignmentlist.dart
@@ -3,8 +3,9 @@ import 'package:flutter/material.dart';
 class Assignmentss {
   final String title;
   final String dueDate;
+  final String url;
 
-  Assignmentss(this.title, this.dueDate);
+  Assignmentss(this.title, this.dueDate,this.url);
 }
 
 class Assignmentlist extends StatefulWidget {
@@ -17,20 +18,20 @@ class Assignmentlist extends StatefulWidget {
 class _HomePageState extends State<Assignmentlist> {
   // Example data (you can replace this with your actual data)
   final List<Assignmentss> assignments = [
-    Assignmentss('TAFL Homework', 'October 25, 2021'),
-    Assignmentss('OPPs Project', 'November 15, 2021'),
-    Assignmentss('DSA Homework', 'October 25, 2021'),
-    Assignmentss('DBMS Homework', 'October 25, 2021'),
-    Assignmentss('TAFL Homework', 'October 25, 2021'),
-    Assignmentss('TAFL Homework', 'October 25, 2021'),
-    Assignmentss('TAFL Homework', 'October 25, 2021'),
-    Assignmentss('TAFL Homework', 'October 25, 2021'),
-    Assignmentss('OPPs Project', 'November 15, 2021'),
-    Assignmentss('DSA Homework', 'October 25, 2021'),
-    Assignmentss('DBMS Homework', 'October 25, 2021'),
-    Assignmentss('TAFL Homework', 'October 25, 2021'),
-    Assignmentss('TAFL Homework', 'October 25, 2021'),
-    Assignmentss('TAFL Homework', 'October 25, 2021'),
+    Assignmentss('TAFL Homework', 'October 25, 2021','assets/images/TAFL.png'),
+    Assignmentss('OPPs Project', 'November 15, 2021','assets/images/OOPs.png'),
+    Assignmentss('DSA Homework', 'October 25, 2021','assets/images/DSA.png'),
+    Assignmentss('DBMS Homework', 'October 25, 2021','assets/images/DBMS.png'),
+    Assignmentss('TAFL Homework', 'October 25, 2021','assets/images/TAFL.png'),
+    Assignmentss('TAFL Homework', 'October 25, 2021','assets/images/TAFL.png'),
+    Assignmentss('TAFL Homework', 'October 25, 2021','assets/images/TAFL.png'),
+    Assignmentss('TAFL Homework', 'October 25, 2021','assets/images/TAFL.png'),
+    Assignmentss('OPPs Project', 'November 15, 2021','assets/images/OOPs.png'),
+    Assignmentss('DSA Homework', 'October 25, 2021','assets/images/DSA.png'),
+    Assignmentss('DBMS Homework', 'October 25, 2021','assets/images/DBMS.png'),
+    Assignmentss('TAFL Homework', 'October 25, 2021','assets/images/TAFL.png'),
+    Assignmentss('TAFL Homework', 'October 25, 2021','assets/images/TAFL.png'),
+    Assignmentss('TAFL Homework', 'October 25, 2021','assets/images/TAFL.png'),
 
     // Add more assignments here
   ];
@@ -63,19 +64,25 @@ class _HomePageState extends State<Assignmentlist> {
               'Due: ${assignment.dueDate}',
               style: TextStyle(fontSize: 15),
             ),
-            trailing: ElevatedButton(
-              onPressed: () {},
-              child: Text(
-                "Marl as Completed",
-                style: TextStyle(color: Colors.white),
+            trailing: SizedBox(
+              width: 110,
+              height: 35,
+              child: ElevatedButton(
+                onPressed: () {},
+                child: Text(
+                  "Mark as Completed",
+                  style: TextStyle(color: Colors.white,fontSize: 10),
+                ),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.green,
+                  padding: EdgeInsets.all(0),
+                )
               ),
-              style: ButtonStyle(
-                backgroundColor: MaterialStatePropertyAll(Colors.green),
-                shape: MaterialStatePropertyAll<RoundedRectangleBorder>(
-                    RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(18),
-                )),
-              ),
+            ),
+            leading: SizedBox(
+              height:100,
+              width: 70,
+              child: Image.asset(assignment.url,fit: BoxFit.cover,),
             ),
           );
         },
