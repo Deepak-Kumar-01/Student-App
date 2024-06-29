83f1128b84ac3f05c66df57de427c62c38177641
Deepak-Kumar-224 <deepakkumar224336@gmail.com>
2024-06-29 18:49:07 +0530

    flutter sdk updated

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\src\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\ramas\AndroidStudioProjects\Student-App"
+export "FLUTTER_APPLICATION_PATH=C:\Users\deepa\StudioProjects\JSS\Student-App"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
