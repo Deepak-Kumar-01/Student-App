3c0f3eb47bba0760850f5e922361c5b273d89964
Mks30d <manish30d@gmail.com>
2024-07-01 15:37:53 +0530

    Implemented a horizontal scrollable list of the next 7 days and display the routine details

--- lib/screens/Routine/timetable.dart
+++ lib/screens/Routine/timetable.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
 import 'package:studentapp/screens/Routine/timetable_api.dart';
 
 class TimeTable extends StatefulWidget {
@@ -14,52 +16,88 @@ class _TimeTableState extends State<TimeTable> {
   var room = routineapi["monday"]?[1]["room"];
   @override
   Widget build(BuildContext context) {
+    var dateTime = DateTime.now();
 
-    return Container(
-      width: double.infinity,
-      height: double.infinity,
-      decoration: BoxDecoration(
-        color: Color(0xFFE5E5EC),
-      ),
-      child: ListView.builder(
-        itemCount: routineapi["monday"]?.length,
-        itemBuilder: (context, index) {
+    // Generate a list of the next 5 days including the current date
+    List<DateTime> nextDays = List.generate(7, (index) {
+      return dateTime.add(Duration(days: index));
+    });
 
-          var subjectTime = routineapi["monday"]?[index]["time"];
-          var subjectCode = routineapi["monday"]?[index]["subjectCode"];
-          var subjectName = routineapi["monday"]?[index]["subjectName"];
-          var subjectTeacher = routineapi["monday"]?[index]["subjectTeacher"];
+    return Scaffold(
+      body: Column(
+        children: [
+          Container(
+            height: 100,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: nextDays.length,
+              itemBuilder: (context, index) {
+                DateTime day = nextDays[index]; // current DateTime details
 
-          return Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(
-                    left: 9, right: 9, top: 5, bottom: 4),
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(6),
+                return InkWell(
+                  onTap: (){
+                    print("$index");
+                  },
+                  child: Container(
+                    width: 80,
+                    color: Colors.blueAccent,
+                    alignment: Alignment.center,
+                    child: Text("${DateFormat("dd").format(day)}\n${DateFormat("E").format(day)}"),
                   ),
-                  child: ListTile(
-                    leading: Text(
-                      subjectTime!,
-                      style: TextStyle(fontSize: 12),
-                    ),
-                    title: Text(
-                      "$subjectName ($subjectCode)",
-                      style: TextStyle(fontWeight: FontWeight.bold),
-                    ),
-                    subtitle: Text(subjectTeacher!,
-                        style: TextStyle(color: Color(0xFF5E5E5E))),
-                  ),
-                ),
+                );
+              },
+            ),
+          ),
+          Expanded(
+            child: Container(
+              // width: double.infinity,
+              // height: 200,
+              decoration: BoxDecoration(
+                color: Color(0xFFE5E5EC),
               ),
-              // SizedBox(
-              //   height: 5,
-              // )
-            ],
-          );
-        },
+              child: ListView.builder(
+                itemCount: routineapi["monday"]?.length,
+                itemBuilder: (context, index) {
+                  var subjectTime = routineapi["monday"]?[index]["time"];
+                  var subjectCode = routineapi["monday"]?[index]["subjectCode"];
+                  var subjectName = routineapi["monday"]?[index]["subjectName"];
+                  var subjectTeacher =
+                  routineapi["monday"]?[index]["subjectTeacher"];
+
+                  return Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(
+                            left: 9, right: 9, top: 5, bottom: 4),
+                        child: Container(
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(6),
+                          ),
+                          child: ListTile(
+                            leading: Text(
+                              subjectTime!,
+                              style: TextStyle(fontSize: 12),
+                            ),
+                            title: Text(
+                              "$subjectName ($subjectCode)",
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                            ),
+                            subtitle: Text(subjectTeacher!,
+                                style: TextStyle(color: Color(0xFF5E5E5E))),
+                          ),
+                        ),
+                      ),
+                      // SizedBox(
+                      //   height: 5,
+                      // )
+                    ],
+                  );
+                },
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
