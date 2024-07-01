6c1a8cf23d42812c95a607e4eed2c26b3136f00b
Mks30d <manish30d@gmail.com>
2024-07-01 15:30:01 +0530

    Implemented custom API calling in Routine for Student App

--- lib/screens/Routine/timetable.dart
+++ lib/screens/Routine/timetable.dart
@@ -1,5 +1,5 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:studentapp/screens/Routine/timetable_api.dart';
 
 class TimeTable extends StatefulWidget {
   const TimeTable({super.key});
@@ -9,15 +9,11 @@ class TimeTable extends StatefulWidget {
 }
 
 class _TimeTableState extends State<TimeTable> {
+  var subject = routineapi["monday"]?[1]["subject"];
+  var time = routineapi["monday"]?[1]["time"];
+  var room = routineapi["monday"]?[1]["room"];
   @override
   Widget build(BuildContext context) {
-    // return Container(
-    //   child: TextButton(onPressed: (){
-    //     print("$subject, $time, $room");
-    //     print("Clicked...");
-    //   }, child: Text("Click")),
-    // );
-
 
     return Container(
       width: double.infinity,
@@ -26,41 +22,41 @@ class _TimeTableState extends State<TimeTable> {
         color: Color(0xFFE5E5EC),
       ),
       child: ListView.builder(
-        itemCount: 10,
+        itemCount: routineapi["monday"]?.length,
         itemBuilder: (context, index) {
-          // var subjectCode = subjectDetails[index]["subjectCode"];
-          // var subjectName = subjectDetails[index]["subjectName"];
-          // var subjectTeacher = subjectDetails[index]["subjectTeacher"];
-          // var subjectAttendance = studentAttendance[index];
-          var subjectTime = "9:00 am";
-          var subjectCode = "Code";
-          var subjectName = "Subject Name";
-          var subjectTeacher = "Subject Teacher";
-          var subjectAttendance = 90;
+
+          var subjectTime = routineapi["monday"]?[index]["time"];
+          var subjectCode = routineapi["monday"]?[index]["subjectCode"];
+          var subjectName = routineapi["monday"]?[index]["subjectName"];
+          var subjectTeacher = routineapi["monday"]?[index]["subjectTeacher"];
 
           return Column(
             children: [
               Padding(
-                padding: const EdgeInsets.only(left: 11, right: 11, top: 1, bottom: 1),
+                padding: const EdgeInsets.only(
+                    left: 9, right: 9, top: 5, bottom: 4),
                 child: Container(
                   decoration: BoxDecoration(
                     color: Colors.white,
                     borderRadius: BorderRadius.circular(6),
                   ),
                   child: ListTile(
-                    leading: Text(subjectTime, style: TextStyle(fontSize: 12),),
-
-                    title: Text("$subjectName ($subjectCode)", style: TextStyle(fontWeight: FontWeight.bold),),
-
+                    leading: Text(
+                      subjectTime!,
+                      style: TextStyle(fontSize: 12),
+                    ),
+                    title: Text(
+                      "$subjectName ($subjectCode)",
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
                     subtitle: Text(subjectTeacher!,
                         style: TextStyle(color: Color(0xFF5E5E5E))),
-
                   ),
                 ),
               ),
-              SizedBox(
-                height: 5,
-              )
+              // SizedBox(
+              //   height: 5,
+              // )
             ],
           );
         },
