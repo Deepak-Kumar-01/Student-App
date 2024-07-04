fda876776b32c7b2acd134b432442dae11890334
eleminatorcode <115079598+eleminatorcode@users.noreply.github.com>
2024-07-04 15:05:13 +0530

    change password

--- lib/screens/facultyClub/Assignment/assignmentlist.dart
+++ lib/screens/facultyClub/Assignment/assignmentlist.dart
@@ -19,20 +19,22 @@ class Assignmentlist extends StatefulWidget {
 class _HomePageState extends State<Assignmentlist> {
   // Example data (you can replace this with your actual data)
   final List<Assignmentss> assignments = [
-    Assignmentss('TAFL Homework', 'October 25, 2021','assets/images/TAFL.png',Color(0xff7ed1e6)),
-    Assignmentss('OPPs Project', 'November 15, 2021','assets/images/OOPs.png',Color(0xFFfff27a)),
-    Assignmentss('DSA Homework', 'October 25, 2021','assets/images/DSA.png',Color(0xffe1c0b6)),
-    Assignmentss('DBMS Homework', 'October 25, 2021','assets/images/DBMS.png',Color(0xff8ec5b6)),
-    Assignmentss('TAFL Homework', 'October 25, 2021','assets/images/TAFL.png',Color(0xff7ed1e6)),
-    Assignmentss('TAFL Homework', 'October 25, 2021','assets/images/TAFL.png',Color(0xff7ed1e6)),
-    Assignmentss('TAFL Homework', 'October 25, 2021','assets/images/TAFL.png',Color(0xff7ed1e6)),
-    Assignmentss('TAFL Homework', 'October 25, 2021','assets/images/TAFL.png',Color(0xff7ed1e6)),
-    Assignmentss('OPPs Project', 'November 15, 2021','assets/images/OOPs.png',Color(0xFFfff27a)),
-    Assignmentss('DSA Homework', 'October 25, 2021','assets/images/DSA.png',Color(0xffe1c0b6)),
-    Assignmentss('DBMS Homework', 'October 25, 2021','assets/images/DBMS.png',Color(0xff8ec5b6)),
-    Assignmentss('TAFL Homework', 'October 25, 2021','assets/images/TAFL.png',Color(0xff7ed1e6)),
-    Assignmentss('TAFL Homework', 'October 25, 2021','assets/images/TAFL.png',Color(0xff7ed1e6)),
-    Assignmentss('TAFL Homework', 'October 25, 2021','assets/images/TAFL.png',Color(0xff7ed1e6)),
+    Assignmentss('TAFL Homework', 'October 25, 2021','assets/images/autmata.png',Color(0xffff7f3e)),
+    Assignmentss('OPPs Project', 'November 15, 2021','assets/images/Java.png',Color(0xFF7aa2e3)),
+    Assignmentss('DSA Homework', 'October 25, 2021','assets/images/os.png',Color(0xff36ba98)),
+    Assignmentss('DBMS Homework', 'October 25, 2021','assets/images/dbms.png',Color(0xff7071e8)),
+    Assignmentss('TAFL Homework', 'October 25, 2021','assets/images/autmata.png',Color(0xffff7f3e)),
+    Assignmentss('TAFL Homework', 'October 25, 2021','assets/images/autmata.png',Color(0xffff7f3e)),
+    Assignmentss('TAFL Homework', 'October 25, 2021','assets/images/autmata.png',Color(0xffff7f3e)),
+    Assignmentss('TAFL Homework', 'October 25, 2021','assets/images/autmata.png',Color(0xffff7f3e)),
+    Assignmentss('OPPs Project', 'November 15, 2021','assets/images/Java.png',Color(0xFF7aa2e3)),
+    Assignmentss('DSA Homework', 'October 25, 2021','assets/images/os.png',Color(0xff36ba98)),
+    Assignmentss('DBMS Homework', 'October 25, 2021','assets/images/dbms.png',Color(0xff7071e8)),
+    Assignmentss('TAFL Homework', 'October 25, 2021','assets/images/autmata.png',Color(0xffff7f3e)),
+    Assignmentss('TAFL Homework', 'October 25, 2021','assets/images/autmata.png',Color(0xffff7f3e)),
+    Assignmentss('TAFL Homework', 'October 25, 2021','assets/images/autmata.png',Color(0xffff7f3e)),
+    Assignmentss('OS Homework', 'October 25, 2021','assets/images/autmata.png',Color(0xffff6969)),
+
 
     // Add more assignments here
   ];
@@ -54,8 +56,11 @@ class _HomePageState extends State<Assignmentlist> {
         gridDelegate:  SliverGridDelegateWithFixedCrossAxisCount(
           crossAxisCount: 2,
           crossAxisSpacing: 15,
-          mainAxisSpacing: 15, // Number of columns
+          mainAxisSpacing: 15,
+          childAspectRatio: 2.5/3,
+          // Number of columns
         ),
+
 
         padding: EdgeInsets.fromLTRB(10,10,10,0),
         scrollDirection: Axis.vertical,
@@ -63,29 +68,36 @@ class _HomePageState extends State<Assignmentlist> {
         itemBuilder: (context, index) {
           final assignment = assignments[index];
           return ListTile(
+
             title: Padding(
-              padding: const EdgeInsets.fromLTRB(10,8.0,0,0),
-              child: Text(
-                assignment.title,
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-              ),
+              padding: const EdgeInsets.all(8.0),
+              child: Ink.image(image:AssetImage(assignment.url),height: 50,),
             ),
             subtitle: Column(
               children: [
-
+                      Row(
+                      children: [  Padding(
+                          padding: const EdgeInsets.fromLTRB(12,20.0,0,0),
+                          child: Text(
+                            assignment.title,
+                            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20,color: Colors.white),
+                          ),
+                        ),
+          ],
+                      ),
                    Row(
                     children: [
                       Padding(
-                        padding: const EdgeInsets.fromLTRB(8.0,0,0,0),
+                        padding: const EdgeInsets.fromLTRB(11.4,20,0,0),
                         child: Text(
                           'Due: ${assignment.dueDate}',
-                          style: TextStyle(fontSize: 15),
+                          style: TextStyle(fontSize: 15,color: Colors.white),
                         ),
                       ),
                     ],
                   ),
                  Row( children: [ Padding(
-                   padding: const EdgeInsets.fromLTRB(42.0,60,0,0),
+                   padding: const EdgeInsets.fromLTRB(44,14,0,0),
                    child: SizedBox(
                       width: 100,
                       height: 25,
@@ -93,10 +105,10 @@ class _HomePageState extends State<Assignmentlist> {
                           onPressed: () {},
                           child: Text(
                             "Mark as Completed",
-                            style: TextStyle(color: Colors.white,fontSize: 10),
+                            style: TextStyle(color: assignment.c1,fontSize: 10),
                           ),
                           style: ElevatedButton.styleFrom(
-                            backgroundColor: Colors.green,
+                            backgroundColor: Colors.white,
 
                             padding: EdgeInsets.all(5),
                             shadowColor: Colors.grey[850],
@@ -118,7 +130,7 @@ class _HomePageState extends State<Assignmentlist> {
             //),
             tileColor: assignment.c1,
             shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(30),),
+              borderRadius: BorderRadius.circular(20),),
             contentPadding: EdgeInsets.all(4),
 
           );
